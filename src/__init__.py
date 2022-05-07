9f53140082bbc5ffdb9a594b2bb07a7aa843042c
Gustavo Neiva <luizgzn@gmail.com>
2022-05-06 23:47:56 -0300

    rename file and structure

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from .umidity import read_data
--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,10 @@
 import sys
-import umidity
+from .umidity import read_data
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    umidity.read_data()
+    read_data()
 
 if __name__ == "__main__":
     sys.exit(main())
