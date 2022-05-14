d928bb553214c029d067389028f3805093bc1781
Gustavo Neiva <luizgzn@gmail.com>
2022-05-14 17:01:50 -0300

    fix class error:

--- green_controller/relay_controller.py
+++ green_controller/relay_controller.py
@@ -7,11 +7,11 @@ class RelayController:
     [ GPIO.setup(gpio_id, GPIO.OUT) for gpio_id in gpio_ids ]
     return RelayController()
 
-  def on(gpio_id):
+  def on(self, gpio_id):
     GPIO.output(gpio_id, GPIO.LOW)
 
-  def off(gpio_id):
+  def off(self, gpio_id):
     GPIO.output(gpio_id, GPIO.HIGH)
 
-  def cleanup():
+  def cleanup(self):
     GPIO.cleanup()
