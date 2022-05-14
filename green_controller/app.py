3860892fd82b0ce3a0c16f4f060ccd21aee80086
Gustavo Neiva <luizgzn@gmail.com>
2022-05-14 13:50:53 -0300

    add conditional to temperature check

--- green_controller/app.py
+++ green_controller/app.py
@@ -21,39 +21,39 @@ class GreenController:
 
   def run(self):
     humidity, temperature = read()
+    if humidity is not None and temperature is not None:
+      self.temperatures.append(temperature)
+      self.humidities.append(humidity)
+      
+      if temperature >= 25:
+        self.relay.on(RELAIS_1_GPIO)
+        self.relay.on(RELAIS_2_GPIO)
+        self.relay.off(RELAIS_3_GPIO)
+        self.relay.off(RELAIS_4_GPIO)
+      if temperature > 22 and temperature < 25:
+        self.relay.off(RELAIS_1_GPIO)
+        self.relay.off(RELAIS_2_GPIO)
+        self.relay.on(RELAIS_3_GPIO)
+        self.relay.on(RELAIS_4_GPIO)
 
-    self.temperatures.append(temperature)
-    self.humidities.append(humidity)
-    
-    if temperature >= 25:
-      self.relay.on(RELAIS_1_GPIO)
-      self.relay.on(RELAIS_2_GPIO)
-      self.relay.off(RELAIS_3_GPIO)
-      self.relay.off(RELAIS_4_GPIO)
-    if temperature > 22 and temperature < 25:
-      self.relay.off(RELAIS_1_GPIO)
-      self.relay.off(RELAIS_2_GPIO)
-      self.relay.on(RELAIS_3_GPIO)
-      self.relay.on(RELAIS_4_GPIO)
+      temp = f'Temp.={temperature:0.2f}*C'
+      umidade = f'Umidade={humidity:0.2f}%'
+      print(f'{temp} {umidade}')
 
-    temp = f'Temp.={temperature:0.2f}*C'
-    umidade = f'Umidade={humidity:0.2f}%'
-    print(f'{temp} {umidade}')
+      self.display.lcd_display_string(temp, 1)
+      self.display.lcd_display_string(umidade, 2) 
+      self.counter += 1
 
-    self.display.lcd_display_string(temp, 1)
-    self.display.lcd_display_string(umidade, 2) 
-    self.counter += 1
-
-    if self.counter >= 130:
-      humidity_medium = sum(humidities) / len(humidities)
-      temp_medium = sum(temperatures) / len(temperatures)
-      temp_medium_string = f'Temp.={temp_medium:0.2f}*C'
-      humidity_medium_string = f'Umidade={humidity_medium:0.2f}%'
-      print('****** MEDIA ******')
-      print(f'{temp_medium_string} {humidity_medium_string}')
-      temperatures = []
-      humidities = []
-      self.counter = 0
+      if self.counter >= 130:
+        humidity_medium = sum(humidities) / len(humidities)
+        temp_medium = sum(temperatures) / len(temperatures)
+        temp_medium_string = f'Temp.={temp_medium:0.2f}*C'
+        humidity_medium_string = f'Umidade={humidity_medium:0.2f}%'
+        print('****** MEDIA ******')
+        print(f'{temp_medium_string} {humidity_medium_string}')
+        temperatures = []
+        humidities = []
+        self.counter = 0
 
   def stop(self):
     sleep(1)
