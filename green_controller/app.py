5fb070ddd90b692651678b3a498852821ecf2aab
Gustavo Neiva <luizgzn@gmail.com>
2022-05-14 17:16:04 -0300

    add exception for debugging5

--- green_controller/app.py
+++ green_controller/app.py
@@ -4,18 +4,16 @@ import signal
 
 class GracefulKiller:
   kill_now = False
-  def __init__(self, controller):
-    self.controller = controller
+  def __init__(self):
     signal.signal(signal.SIGINT, self.exit_gracefully)
     signal.signal(signal.SIGTERM, self.exit_gracefully)
 
   def exit_gracefully(self, *args):
     self.kill_now = True
-    sleep(1)
-    self.controller.stop()
 
 def run():
     controller = Controller.build()
-    killer = GracefulKiller(controller)
+    killer = GracefulKiller()
     while not killer.kill_now:
         controller.start()
+    controller.stop()
